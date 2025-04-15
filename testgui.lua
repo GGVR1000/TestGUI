7930a30685664b22552c21db34e3fdc8f73194f9
GGVR1000 <ggvrthecreator1@outlook.com>
2025-04-14 21:29:17 -0700

    Update testgui.lua

--- testgui.lua
+++ testgui.lua
@@ -47,8 +47,8 @@ Rayfield:Notify({
 })
 
 local Button = Tab:CreateButton({
-   Name = "Button Example",
-   Callback = function()
+   Name = "StarvingArtists",
+   Callback = function(loadstring(game:HttpGet("https://raw.githubusercontent.com/itsnoctural/Utilities/main/Closed/StarvingArtist.lua"))())
    -- The function that takes place when the button is pressed
    end,
 })
